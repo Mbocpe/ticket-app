688e6207b8007222ca2726b18abb29590cae067b
Mbocpe <bocminhkhoi@gmail.com>
2026-01-22 12:36:35 +1000

    Transform data to UI

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,41 @@
 import TicketCard from "./(components)/TicketCard"
 
-const Dashboard = () => {
+const getTickets = async () => {
+  try {
+    const res = await fetch("http://localhost:3000/api/Tickets", {
+      cache: "no-store"
+    })
+    return res.json()
+  } catch (err) {
+    console.log("Failed to get tickets", err);
+  }
+}
+
+const Dashboard = async () => {
+  const {tickets} = await getTickets();
+  const uniqueCategories = [
+    ... new Set(tickets?.map(({category}) => category))
+  ]
+
   return (
     <div className="p-5">
-      <div className="lg:grid grid-cols-2 xl:grid-cols-4">
-        <TicketCard/>
-        <TicketCard/>
+      <div>
+        {tickets && uniqueCategories?.map((uniqueCategory, categoryIndex) => (
+          <div className="mb-4" key={categoryIndex}>
+            <h2>{uniqueCategory}</h2>
+
+            <div className="lg:grid grid-cols-2 xl:grid-cols-4">
+
+              {/* Tickets are filtered based on category and only shown if there are tickets in that category */}
+              {tickets
+                .filter((ticket) => ticket.category === uniqueCategory)
+                .map((filteredTicket, _index) => (
+                  <TicketCard id={_index} key={_index} ticket={filteredTicket}/>
+                ))
+              }
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   )
