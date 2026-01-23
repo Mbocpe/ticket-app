35483b44bcb3589047a85bcca134d6d8956c2d06
Mbocpe <bocminhkhoi@gmail.com>
2026-01-23 14:59:06 +1000

    Delete tickets

---
+++ app/api/Tickets/[id]/route.js
@@ -0,0 +1,13 @@
+import Ticket from "@/app/(models)/Ticket";
+import { NextResponse } from "next/server";
+
+export async function DELETE(req, {params}) {
+    try {
+        const {id} = await params;
+        await Ticket.findByIdAndDelete(id);
+
+        return NextResponse.json({message: "Successful deletion"}, {status: 200});
+    } catch (err) {
+        return NextResponse.json({message: "Error", err}, {status: 500});
+    }
+}
